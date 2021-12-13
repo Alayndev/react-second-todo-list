517483e0f98f4fcb322c651e3096858f93a920be
alayn saez <saezalayn@gmail.com>
2021-12-13 10:44:56 -0300

    github pages update

--- src/hooks/useStorageListener.jsx
+++ src/hooks/useStorageListener.jsx
@@ -3,26 +3,27 @@ import { useState } from "react";
 // HOC a Custom Hook
 export function useStorageListener(syncBrowsertab) {
   const [storageChange, setStorageChange] = useState(false); 
-
+  
   window.addEventListener("storage", (change) => {
     if (change.key === "TODOS_V2") {
       console.log(`Hubo cambios en ${change.key}`);
-
+      
       setStorageChange(true);
     }
   });
-
+  
   const toggleShow = () => {
     syncBrowsertab();
     setStorageChange(false);
   };
-
+  
   return {
     show: storageChange,
     toggleShow,
   };
 }
 
+// HOC a Custom Hook:
 // 1) Cambiamos el nombre de with... a use...
 // 2) Sacamos las funciones internas y dejamos solamente la lógica
 // 3) Devolvemos las propiedades a compartir en el return
